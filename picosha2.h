9c2a9feed3a5dc7967b1b8131d9ec9a983fee1b5
okdshin <kokuzen@gmail.com>
2017-05-13 21:35:44 +0900

    update 2014->2017

--- picosha2.h
+++ picosha2.h
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (C) 2014 okdshin
+Copyright (C) 2017 okdshin
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal

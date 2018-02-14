257421ee36d362006097d5d018242ecb67134ef8
Aleksey Sergey <aleksey.sergey@tomtom.com>
2018-02-14 15:48:43 +0100

    renamed KDigestSize into k_digest_size

--- picosha2.h
+++ picosha2.h
@@ -40,7 +40,7 @@ namespace picosha2 {
 typedef unsigned long word_t;
 typedef unsigned char byte_t;
 
-static const size_t KDigestSize = 32;
+static const size_t k_digest_size = 32;
 
 namespace detail {
 inline byte_t mask_8bit(byte_t x) { return x & 0xff; }
@@ -273,9 +273,9 @@ class hash256_one_by_one {
 
 inline void get_hash_hex_string(const hash256_one_by_one& hasher,
                                 std::string& hex_str) {
-    byte_t hash[KDigestSize];
-    hasher.get_hash_bytes(hash, hash + KDigestSize);
-    return bytes_to_hex_string(hash, hash + KDigestSize, hex_str);
+    byte_t hash[k_digest_size];
+    hasher.get_hash_bytes(hash, hash + k_digest_size);
+    return bytes_to_hex_string(hash, hash + k_digest_size, hex_str);
 }
 
 inline std::string get_hash_hex_string(const hash256_one_by_one& hasher) {
@@ -342,10 +342,10 @@ void hash256(const InContainer& src, OutContainer& dst) {
 
 template <typename InIter>
 void hash256_hex_string(InIter first, InIter last, std::string& hex_str) {
-    byte_t hashed[KDigestSize];
-    hash256(first, last, hashed, hashed + KDigestSize);
+    byte_t hashed[k_digest_size];
+    hash256(first, last, hashed, hashed + k_digest_size);
     std::ostringstream oss;
-    output_hex(hashed, hashed + KDigestSize, oss);
+    output_hex(hashed, hashed + k_digest_size, oss);
     hex_str.assign(oss.str());
 }
 
